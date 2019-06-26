1737e5af08b16704a289034beb879d748722eba4
Sneha Thadani <snehacopper@gmail.com>
2019-06-25 22:10:03 -0700

    functional component working need polishing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-
+import Login from './Components/Login';
 import './App.css';
-
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount() {
+    const a= localStorage.getItem("rememberMe")===true;
+    const b = a ? localStorage.getItem("userName"):"";
+    this.setState({a,b});
+  }
+  render(){
   return (
     <div className="App">
     
-     
+     <Login/>
     </div>
   );
+}
 }
 
 export default App;
